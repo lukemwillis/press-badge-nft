cd6182474b846ee50cd6df8f746a04228874a356
luke <luke@prescryptive.com>
2023-04-21 16:13:26 -0700

    hex metadata

--- nft/assembly/Constants.ts
+++ nft/assembly/Constants.ts
@@ -5,7 +5,7 @@ export namespace Constants {
   export const SYMBOL: string = "KPB";
   export const PRICE: u64 = 50000000000;
   export const MAX: u64 = 50;
-  export const URI: string = "ipfs://bafybeiaiynisgaa3eqf3bxuseh4yh4nzjz4g3nvocoh3zlz7eypjx3p5wm";
+  export const URI: string = "ipfs://bafybeierlyjjivgfuzu2ap2bjjh7aan66gk4a2hoj4ikfyikq4sforzk3a";
   export const ROYALTIES: u64 = 0;
 
   let contractId: Uint8Array | null = null;
